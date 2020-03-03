676602e5ace967f4eb9bf50d1f16bbabecb81190
Oscar <mundonatural@gmail.com>
2020-03-03 18:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,15 @@ function increaseRankBy(n) {
     }
   }
 }
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+}
