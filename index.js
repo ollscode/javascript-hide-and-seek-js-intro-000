ce990cf112e8dcd928e2dea73751e43bd9bb1431
Oscar <mundonatural@gmail.com>
2020-03-03 18:16:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 function getFirstSelector(s) {
   return document.querySelector(s);
 }
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
